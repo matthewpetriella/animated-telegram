bdb6f7616612ae8b87c57c262c6af4239f949dd3
sper0054 <teresa.sperl@gmail.com>
2022-05-08 11:10:08 -0500

    client side utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,53 +1,40 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql`
+export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
-        username
       }
     }
   }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
       token
       user {
         _id
-        username
       }
     }
   }
 `;
 
-export const ADD_FRIEND = gql`
-  mutation addFriend($id: ID!) {
-    addFriend(friendId: $id) {
-      _id
-      username
-      friendCount
-      friends {
-        _id
-        username
-      }
-    }
-  }
-`;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
+export const ADD_ORDER = gql`
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      purchaseDate
+      products {
         _id
+        name
+        price
+        quantity
+        category {
+          name
+        }
       }
     }
   }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,97 +1,100 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
+//export const QUERY_THOUGHTS = gql`
+  //query thoughts($username: String) {
+    //thoughts(username: $username) {
+     // _id
+      //thoughtText
+      //createdAt
+      //username
+      //reactionCount
+      //reactions {
+        //_id
+       // createdAt
+       // username
+        //reactionBody
+      //}
+    //}
+  //}
+//`;
+//export const QUERY_THOUGHT = gql`
+  //query thought($id: ID!) {
+    //thought(_id: $id) {
+     // _id
+      //thoughtText
+      //createdAt
+      //username
+      //reactionCount
+      //reactions {
+       // _id
+        //createdAt
+        //username
+        //reactionBody
+      //}
+    //}
+  //}
+//`;
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
       _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
+      name
+      price
+      quantity
+      image
+      category {
         _id
-        createdAt
-        username
-        reactionBody
       }
     }
   }
 `;
-export const QUERY_THOUGHT = gql`
-  query thought($id: ID!) {
-    thought(_id: $id) {
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
       _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
+      name
+      price
+      quantity
+      category {
+        name
       }
     }
   }
 `;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
 
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      friendCount
-      friends {
-        _id
-        username
-      }
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-        reactionCount
-      }
-    }
-  }
-`;
-export const QUERY_ME = gql`
   {
-    me {
-      _id
-      username
-      email
-      friendCount
-      thoughts {
+    user {
+      orders {
         _id
-        thoughtText
-        createdAt
-        reactionCount
-        reactions {
+        purchaseDate
+        products {
           _id
-          createdAt
-          reactionBody
-          username
+          name
+          price
+          quantity
+          image
         }
-      }
-      friends {
-        _id
-        username
-      }
-    }
-  }
-`;
-
-export const QUERY_ME_BASIC = gql`
-  {
-    me {
-      _id
-      username
-      email
-      friendCount
-      friends {
-        _id
-        username
       }
     }
   }
